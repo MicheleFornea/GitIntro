cb789a7ac67d2e1f2606bd2202c23e3b5a854637
Michele Fornea <michele_fornea@hotmail.it>
2017-06-12 11:57:58 -0400

    C3: Test identities

---
+++ identities.py
@@ -0,0 +1,8 @@
+from fibonacci import fibo as F
+
+N=11
+print("Testing the identity sum sum_{n=1}^N F_n=F_{N+2}-1")
+
+print("For N={}".format(N))
+print("LHS="+str(sum([F(n) for n in range(1,N+1)])))
+print("RHS="+str(F(N+2)-1))
